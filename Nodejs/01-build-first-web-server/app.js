8bb54222e461c38c09fb24efa9bc6b99fbf84988
Robb <hxh.robb@gmail.com>
2017-08-30 13:51:13 +0800

    First commit

---
+++ Nodejs/01-build-first-web-server/app.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req,resp) => {
+    /* // ES6 features? -> "{ <properties> } = <object>"
+    const { method, url } = req;
+    const { headers } = req;
+    const userAgent = headers['user-agent'];
+    console.log(`request = ${method},${url},${userAgent}`);
+    */
+    
+    resp.statusCode = 200;
+    resp.setHeader('Content-Type','text/plain; charset=utf-8');
+    resp.end('hello, world\n');
+});
+
+server.listen(
+    port, hostname,
+    () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    }
+);
